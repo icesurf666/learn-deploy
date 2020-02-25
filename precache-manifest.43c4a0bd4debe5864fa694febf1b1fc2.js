8ceff031103f40538c8fd1b65d336b8416850aab
Pavel Kazantsev <mac@torch-2.local>
2020-02-25 19:15:21 +0700

    Updates

---
+++ precache-manifest.43c4a0bd4debe5864fa694febf1b1fc2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d132a8eb73666e74be4a8f6ff748a936",
+    "url": "/learn-deploy/index.html"
+  },
+  {
+    "revision": "6840b96e5275d8fcdf58",
+    "url": "/learn-deploy/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "0c230c754e7b3149351a",
+    "url": "/learn-deploy/static/js/2.42669e00.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/learn-deploy/static/js/2.42669e00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6840b96e5275d8fcdf58",
+    "url": "/learn-deploy/static/js/main.70c9f2ee.chunk.js"
+  },
+  {
+    "revision": "0568cbd0818cc6251199",
+    "url": "/learn-deploy/static/js/runtime-main.4cd28e0e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/learn-deploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
